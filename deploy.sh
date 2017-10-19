427335fea9c7f9dfce2903928861cc54b17f8368
jean-luc.rakotovao <jean-luc.rakotovao@alterway.fr>
2017-10-19 11:07:05 +0200

    fix cron name

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-/usr/bin/apt-get --assume-yes install unattended-upgrades
+which unattended-upgrades || /usr/bin/apt-get --assume-yes install unattended-upgrades
 
 cp 02periodic /etc/apt/apt.conf.d/02periodic
 cp 50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades
 cp local /etc/apt/apt.conf.d/local
-cp snpgroup$1 /etc/cron.d/snpgroup$1
+cp unattended$1 /etc/cron.d/unattented$1
 
 
 cp required-reboot.sh /usr/local/sbin/required_reboot.sh
