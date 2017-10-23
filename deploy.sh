0c33270d2905761ed07b979e100d70abfc431d00
jean-luc.rakotovao <jean-luc.rakotovao@alterway.fr>
2017-10-23 14:37:54 +0200

    error messages !

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@
 
 which unattended-upgrades || /usr/bin/apt-get --assume-yes install unattended-upgrades
 
-cp 02periodic /etc/apt/apt.conf.d/02periodic
-cp 50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades
-cp local /etc/apt/apt.conf.d/local
-cp unattended$1 /etc/cron.d/unattented$1
+cp 02periodic /etc/apt/apt.conf.d/02periodic || echo "!!! ERREOR !!!"
+cp 50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades || echo "!!! ERREOR !!!"
+cp local /etc/apt/apt.conf.d/local || echo "!!! ERREOR !!!"
+cp unattended$1 /etc/cron.d/unattented$1 || echo "!!! ERREOR !!!"
 
+cp required-reboot.sh /usr/local/sbin/required_reboot.sh  || echo "!!! ERREOR !!!"
+cp required-reboot.conf /etc/zabbix/zabbix_agentd.conf.d.local/required-reboot.conf || echo "!!! ERREOR !!!"
 
-cp required-reboot.sh /usr/local/sbin/required_reboot.sh
-cp required-reboot.conf /etc/zabbix/zabbix_agentd.conf.d.local/required-reboot.conf
 /etc/init.d/zabbix-agent restart
